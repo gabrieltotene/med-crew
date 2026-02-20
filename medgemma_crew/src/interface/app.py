10016ba8e2ba706ba25558102c4613e59fff5d9c
Gabriel Totene <totene22@gmail.com>
2026-02-20 16:10:19 -0300

    implementando interface simples

--- medgemma_crew/src/interface/app.py
+++ medgemma_crew/src/interface/app.py
@@ -1,9 +1,18 @@
 from gradio import Interface,Image, File
+from pathlib import Path 
 from medgemma_crew.main import run
 
 
 
-interface = Interface(fn=run, inputs=[Image(type="filepath")], outputs=[File(type="filepath")], title="Medgemma Crew")
+interface = Interface(
+    fn=run, 
+    inputs=[Image(type="filepath")], 
+    outputs=[File(type="filepath")], 
+    title="Medgemma Crew", 
+    flagging_dir=Path(__file__).parent / "logs",
+    api_visibility="private",
+
+)
 
 interface.launch()
 
