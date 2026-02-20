a5b5adfaa36559ba208e9dfcd7be89523851f907
Lucas-Abner <lucascaixeta02@gmail.com>
2026-02-20 14:59:19 -0300

    Realizado criação de nova tool para criação de docx

--- medgemma_crew/src/medgemma_crew/main.py
+++ medgemma_crew/src/medgemma_crew/main.py
@@ -18,54 +18,14 @@ def run():
     Run the crew.
     """
 
-    img = "/home/lucas.abner/Documentos/code/med-crew/raio_3.png"
+    img = "/home/lucas.abner/Documentos/code/med-crew/raio_4.png"
     img2 = "C:\\Users\\asus\\Documents\\IA\\med-crew\\raio_4.png"
 
     inputs = {
-        'image_url': img2
+        'image_url': img
     }
     
     try:
         MedgemmaCrew().crew().kickoff(inputs=inputs)
     except Exception as e:
         raise Exception(f"An error occurred while running the crew: {e}")
-
-
-def train():
-    """
-    Train the crew for a given number of iterations.
-    """
-    inputs = {
-        "topic": "AI LLMs",
-        'current_year': str(datetime.now().year)
-    }
-    try:
-        MedgemmaCrew().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while training the crew: {e}")
-
-def replay():
-    """
-    Replay the crew execution from a specific task.
-    """
-    try:
-        MedgemmaCrew().crew().replay(task_id=sys.argv[1])
-
-    except Exception as e:
-        raise Exception(f"An error occurred while replaying the crew: {e}")
-
-def test():
-    """
-    Test the crew execution and returns the results.
-    """
-    inputs = {
-        "topic": "AI LLMs",
-        "current_year": str(datetime.now().year)
-    }
-    
-    try:
-        MedgemmaCrew().crew().test(n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while testing the crew: {e}")
