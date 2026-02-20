fef1872db0e293c7a5d19354c681a9f6f4e42de3
Gabriel Totene <totene22@gmail.com>
2026-02-20 15:14:40 -0300

    Adicionando comentario ensinando como rodar o sistem com gradio no app.py do diretorio interface

--- medgemma_crew/src/interface/app.py
+++ medgemma_crew/src/interface/app.py
@@ -6,3 +6,7 @@ from medgemma_crew.main import run
 interface = Interface(fn=run, inputs=[Image(type="filepath")], outputs=[File(type="filepath")], title="Medgemma Crew")
 
 interface.launch()
+
+# Pra rodar
+# cd medgemma_crew/src/
+# python -m interface.app
